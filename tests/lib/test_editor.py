9045b027b6255a107640a5bb1bacdd8222ad28ed
Azat Akhmetov <51468504+metov@users.noreply.github.com>
2021-04-29 21:38:42 -0700

    Fix #1: Incorrect temp file path when empty name + tests (#2)
    * bugfix: empty file name mishandled due to precedence, see #1
    
    * move to subdir and add tests for envars
    
    * test for bug #1 (incorrect temp path when empty name)
    
    * bump version

---
+++ tests/lib/test_editor.py
@@ -0,0 +1,41 @@
+import lib
+import pytest
+
+MOCK_VISUAL = "some_visual"
+MOCK_EDITOR = "some_editor"
+
+
+@pytest.fixture
+def mock_set_visual(monkeypatch):
+    monkeypatch.setenv("VISUAL", MOCK_VISUAL)
+
+
+@pytest.fixture
+def mock_unset_visual(monkeypatch):
+    monkeypatch.delenv("VISUAL")
+
+
+@pytest.fixture
+def mock_set_editor(monkeypatch):
+    monkeypatch.setenv("EDITOR", MOCK_EDITOR)
+
+
+@pytest.fixture
+def mock_unset_editor(monkeypatch):
+    monkeypatch.delenv("EDITOR")
+
+
+def test_both_set(mock_set_visual, mock_set_editor):
+    assert lib.editor() == MOCK_VISUAL
+
+
+def test_visual_only(mock_set_visual, mock_unset_editor):
+    assert lib.editor() == MOCK_VISUAL
+
+
+def test_editor_only(mock_unset_visual, mock_set_editor):
+    assert lib.editor() == MOCK_EDITOR
+
+
+def test_neither_set_raises_EditorError(mock_unset_visual, mock_unset_editor):
+    pytest.raises(lib.EditorError, lib.editor)
